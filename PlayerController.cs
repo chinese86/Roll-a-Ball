7f0daaffddfbaf32c83d4b067685ef969d3f66db
chinese86 <jerry_wong_cal@yahoo.es>
2025-11-26 18:16:51 +0000

    Add files via upload

---
+++ PlayerController.cs
@@ -0,0 +1,72 @@
+using UnityEngine;
+using UnityEngine.InputSystem;
+using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+
+public class PlayerController : MonoBehaviour
+{
+
+    public float speed = 0; 
+    public TextMeshProUGUI countText;
+    private Rigidbody rb;
+    private int count;
+    public GameObject winTextObject;
+    private float movementX;
+    private float movementY;
+    
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+        count = 0; 
+
+        SetCountText();
+        winTextObject.SetActive(false);
+    }
+
+    void OnMove(InputValue movementValue)
+    {
+
+        Vector2 movementVector = movementValue.Get<Vector2>(); 
+        movementX = movementVector.x; 
+        movementY = movementVector.y;
+    }
+       void SetCountText() 
+   {
+       countText.text =  "Count: " + count.ToString();
+         if (count >= 12)
+       {
+           winTextObject.SetActive(true);
+           Destroy(GameObject.FindGameObjectWithTag("Enemy"));
+       }
+   }
+
+    void FixedUpdate()
+    {
+        Vector3 movement = new Vector3 (movementX, 0.0f, movementY);
+        rb.AddForce(movement * speed); 
+    }
+
+     void OnTriggerEnter(Collider other) 
+   {
+      if (other.gameObject.CompareTag("PickUp")) 
+       {
+        other.gameObject.SetActive(false);
+        count = count + 1;
+
+        SetCountText();
+       }
+    
+   }
+   private void OnCollisionEnter(Collision collision)
+{
+   if (collision.gameObject.CompareTag("Enemy"))
+   {
+       // Destroy the current object
+       Destroy(gameObject); 
+       // Update the winText to display "You Lose!"
+       winTextObject.gameObject.SetActive(true);
+       winTextObject.GetComponent<TextMeshProUGUI>().text = "You Lose!";
+   }
+}
+}
